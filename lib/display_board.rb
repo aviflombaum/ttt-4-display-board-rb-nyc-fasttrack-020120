2e00eaabe86414c1de6f1865115601624ce1e2eb
Avi Flombaum <avi@flombaum.com>
2020-02-01 18:37:03 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = [" ", " ", " " ]
+board = ["X", " ", " "," ", "O", " " ," ", " ", " "  ]
+
+def display_board(the_board)
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+
+end
